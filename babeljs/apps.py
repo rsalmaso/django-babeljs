6b963aa36b1d25a18384a1640549e9dcd9533875
Raffaele Salmaso <raffaele@salmaso.org>
2017-07-19 09:12:08 +0200

    renamed app config

--- babeljs/apps.py
+++ babeljs/apps.py
@@ -25,6 +25,10 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class Config(AppConfig):
+class BabelJSConfig(AppConfig):
     name = "babeljs"
     verbose_name = _("BabelJS")
+
+
+# compat
+Config = BabelJSConfig
